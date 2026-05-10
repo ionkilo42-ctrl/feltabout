800c95403c44a38b2cf004d6bf9e1f7d63cf233b
Jonathan <bonvoyagepathseeker@gmail.com>
2026-05-09 22:21:47 -0400

    refactor: remove unnecessary os import in auth.py

--- backend/routers/auth.py
+++ backend/routers/auth.py
@@ -9,7 +9,6 @@ from pydantic import BaseModel, EmailStr
 from sqlalchemy import select
 
 import sys
-import os
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 
 from database import async_session
